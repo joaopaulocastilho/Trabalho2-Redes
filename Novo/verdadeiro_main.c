bc6a0e5b30f61726795eb1d8e9a97fb1b5bf4f5e
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2018-06-30 20:22:55 -0300

    Rascunho da função transmissor.

--- Novo/verdadeiro_main.c
+++ Novo/verdadeiro_main.c
@@ -1,4 +1,5 @@
 #include <stdio.h> // printf
+#include <string.h>
 #include <pthread.h> // Para usar as threads
 #include <unistd.h> // Para usar mutex
 
@@ -27,37 +28,47 @@ typedef struct {
 } pacote_t;
 
 struct argumentos_transmissor_struct {
-  pthread_mutex_t *buffer_saida_mutex,
-  pacote_t *buffer_saida,
-  int tamanho_buffer_saida
-}
+  pthread_mutex_t *buffer_saida_mutex;
+  pacote_t *buffer_saida;
+  int tamanho_buffer_saida;
+  int ultimo_pacote_buffer_saida;
+};
 
-void transmissor(void *argumentos) {
+void *transmissor(void *args) {
+  struct argumentos_transmissor_struct* argumentos = (struct argumentos_transmissor_struct*) args;
   while(1) {
-    printf("Tamanho buffer saída: %d\n", argumentos.tamanho_buffer_saida);
+    printf("Tamanho buffer saída: %d\n", argumentos->tamanho_buffer_saida);
     // pthread_mutex_lock(argumentos->buffer_saida_mutex);
 
     // pthread_mutex_unlock(argumentos->buffer_saida_mutex);
-  return;
+  return NULL;
   }
 }
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("ERRO! Passe o ID do nodo como argumento para execução.\n");
+    return 0;
+  }
+
   int nodo_processo_id = *argv[1] - '0'; // Pega o ID do nó passado por argumento na execução
 
   // Buffer de saída
   pacote_t buffer_saida[TAMANHO_BUFFER_SAIDA];
   pthread_mutex_t buffer_saida_mutex;
-  pthread_mutex_init(&buffer_saida_mutex, NULL); //É ASSIM QUE INICIA UM MUTEX.
+  pthread_mutex_init(&buffer_saida_mutex, NULL);
+  int ultimo_pacote_buffer_saida = 0;
+  memset(buffer_saida, 0, TAMANHO_BUFFER_SAIDA * sizeof(pacote_t));
 
 
   /* Inicia o transmissor **************************************/
   pthread_t transmissor_thread_id;
   struct argumentos_transmissor_struct argumentos_transmissor;
-  argumentos_transmissor.buffer_saida_mutex = buffer_saida_mutex
+  argumentos_transmissor.buffer_saida_mutex = &buffer_saida_mutex;
   argumentos_transmissor.buffer_saida = buffer_saida;
   argumentos_transmissor.tamanho_buffer_saida = TAMANHO_BUFFER_SAIDA;
-  pthread_create(&transmissor_thread_id, NULL, &transmissor, (void*)&argumentos_transmissor);
+  argumentos_transmissor.ultimo_pacote_buffer_saida = ultimo_pacote_buffer_saida;
+  pthread_create(&transmissor_thread_id, NULL, transmissor, (void*)&argumentos_transmissor);
   /*************************************************************/
 
 
