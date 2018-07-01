5606859bb65bfaf73ad2e8094b41497c2a638f40
Igor Lemos Vicente <igorlemosvicente@gmail.com>
2018-07-01 20:48:46 -0300

    Cria função para atualizar o vetor de saltos

---
+++ Novo/atualiza_vetor_saltos.c
@@ -0,0 +1,54 @@
+#ifndef INCLUSAO_ARQUIVO_ATUALIZA_VETOR_SALTOS
+#define INCLUSAO_ARQUIVO_ATUALIZA_VETOR_SALTOS
+#include "globais.h"
+
+/** Atualizador do vetor de saltos.
+ * Função responsável por atualizar o vetor de saltos, que é usado pelo transmissor.
+ * A função deve ser chamada após atualização da tabela de roteamento.
+ * Retorna 1 se houve alguma alteração no vetor de status ou 0 caso não.
+ */
+int atualiza_vetor_saltos(int tabela_roteamento[][QUANTIDADE_MAXIMA_NOS],
+                          pthread_mutex_t *tabela_roteamento_mutex,
+                          int vetor_saltos[],
+                          pthread_mutex_t *vetor_saltos_mutex,
+                          int id_nodo_atual) {
+  int vetor_saltos_local[QUANTIDADE_MAXIMA_NOS];
+  int menor_distancia; // Variável auxiliar
+  int alterou = 0; // Variáveil auxiliar para marcar que teve alteração no vetor de saltos
+
+  memset(vetor_saltos_local, -1, sizeof(vetor_saltos_local));
+
+  pthread_mutex_lock(tabela_roteamento_mutex);
+    for (int no_destino = 0; no_destino < QUANTIDADE_MAXIMA_NOS; no_destino++) {
+      menor_distancia = INFINITO;
+
+      // Se for pra si mesmo, o próximo salto é o próprio nó
+      if (no_destino == id_nodo_atual) {
+        vetor_saltos_local[id_nodo_atual] = id_nodo_atual;
+        continue;
+      }
+
+      for (int linha = 0; linha < QUANTIDADE_MAXIMA_NOS; linha++) {
+        int distancia_pelo_no_linha = tabela_roteamento[linha][id_nodo_atual] + tabela_roteamento[linha][no_destino];
+
+        // Se a distância for menor e o próximo salto não for o próprio nó
+        if (distancia_pelo_no_linha < menor_distancia && linha != id_nodo_atual) {
+          vetor_saltos_local[no_destino] = linha;
+          menor_distancia = distancia_pelo_no_linha;
+        }
+      }
+    }
+  pthread_mutex_unlock(tabela_roteamento_mutex);
+
+  pthread_mutex_lock(vetor_saltos_mutex);
+    for(int i = 0; i < QUANTIDADE_MAXIMA_NOS; i++) {
+      if (vetor_saltos[i] != vetor_saltos_local[i]) {
+        alterou = 1;
+        vetor_saltos[i] = vetor_saltos_local[i];
+      }
+    }
+  pthread_mutex_unlock(vetor_saltos_mutex);
+  return alterou;
+};
+
+#endif
--- Novo/testes_unitarios.c
+++ Novo/testes_unitarios.c
@@ -1,4 +1,5 @@
 #include "globais.h"
+#include "atualiza_vetor_saltos.c"
 
 /* Testa a função converte_pacote_para_char */
 int testa_converte_pacote_para_char() {
@@ -31,6 +32,66 @@ int testa_converte_char_para_pacote() {
   return resultado_comparacao == 0;
 };
 
+int testa_atualiza_vetor_saltos() {
+  int vetor_saltos[QUANTIDADE_MAXIMA_NOS] = {0, 0, 0, 0};
+  pthread_mutex_t vetor_saltos_mutex;
+  int tabela_roteamento[QUANTIDADE_MAXIMA_NOS][QUANTIDADE_MAXIMA_NOS];
+  for(int i = 0; i < QUANTIDADE_MAXIMA_NOS; i++) {
+    for (int j = 0; j < QUANTIDADE_MAXIMA_NOS; j++) {
+      tabela_roteamento[i][j] = INFINITO;
+    }
+  }
+
+  // {0, 5, 3, 6},
+  // {5, 0, 2, 1},
+  // {3, 2, 0, 1},
+  // {4, 1, 1, 0}
+  tabela_roteamento[0][0] = 0;
+  tabela_roteamento[0][1] = 5;
+  tabela_roteamento[0][2] = 3;
+  tabela_roteamento[0][3] = 6;
+  tabela_roteamento[1][0] = 5;
+  tabela_roteamento[1][1] = 0;
+  tabela_roteamento[1][2] = 2;
+  tabela_roteamento[1][3] = 1;
+  tabela_roteamento[2][0] = 3;
+  tabela_roteamento[2][1] = 2;
+  tabela_roteamento[2][2] = 0;
+  tabela_roteamento[2][3] = 1;
+  tabela_roteamento[3][0] = 4;
+  tabela_roteamento[3][1] = 1;
+  tabela_roteamento[3][2] = 1;
+  tabela_roteamento[3][3] = 0;
+
+  pthread_mutex_t tabela_roteamento_mutex;
+
+  // printf("vetor saltos: "); for (int i = 0; i < 4; i++) printf("%d ", vetor_saltos[i]);
+  atualiza_vetor_saltos(tabela_roteamento,
+                        &tabela_roteamento_mutex,
+                        vetor_saltos,
+                        &vetor_saltos_mutex,
+                        0);
+
+  //debug
+  // printf("\n");
+  // for(int i = 0; i < 10; i++) {
+  //   for (int j = 0; j < 10; j++) {
+  //     printf("%10d ", tabela_roteamento[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+  // printf("vetor saltos: "); for (int i = 0; i < 4; i++) printf("%d ", vetor_saltos[i]);
+
+  int vetor_saltos_esperado[4] = {0, 1, 2, 2};
+  for (int i = 0; i < 4; i++) {
+    if (vetor_saltos[i] != vetor_saltos_esperado[i]) {
+      printf("testa_atualiza_vetor_saltos falha em %d\n", i);
+      return 0;
+    }
+  }
+  return 1;
+}
+
 void passou() {
   printf("Passou!\n");
 }
@@ -44,5 +105,7 @@ int main() {
   testa_converte_pacote_para_char() ? passou() : falhou();
   printf("Testando função converte_char_para_pacote: ");
   testa_converte_char_para_pacote() ? passou() : falhou();
+  printf("Testando função testa_atualiza_vetor_saltos: ");
+  testa_atualiza_vetor_saltos() ? passou() : falhou();
   return 0;
 };
